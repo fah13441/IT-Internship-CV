f1be8cc8f33b9ae7a6141f0107dc0fb7c6498ad8
Fahad Fikri <70624971+fah13441@users.noreply.github.com>
2026-04-21 19:24:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const navbar = document.getElementById('navbar');
+        
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.15 
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+            observer.unobserve(entry.target);
+        }
+    });
+}, observerOptions);
+
+document.querySelectorAll('.fade-in-up').forEach(element => {
+    observer.observe(element);
+s});
